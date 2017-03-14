e614fce148dcca06530b7f86a25bbff4d9d0e485
Elayoe <nicoline.larsen@webgolf.dk>
2017-03-14 13:38:21 +0100

    TimeShifting Class now sends message to LevelManager changeTime method

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+	//Used to access varaibles of this class
+	public static LevelManager lm;
+
+	//Test variable
+	public int test = 2;
+
+
+	// Use this for initialization
+	void Start () {
+		lm = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//Receives a 'true' from TimeShifting class if shifting to present, else false
+	public void changeTime(bool present) {
+		//If changing to present
+		if (present) {
+			Debug.Log ("Changing to present time");
+
+			//Change objects
+
+			//Change camera shader
+
+			//Notify TimeShader
+		} 
+		//If changing to past
+		else {
+			Debug.Log ("Changing to past time");
+
+			//Change objects
+
+			//Change camera shader
+
+			//Notify TimeShader
+		}
+	}
+}
---
+++ Assets/Scripts/TimeShifting.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeShifting : MonoBehaviour {
+
+	private bool presentTime = true;
+
+	// Use this for initialization
+	void Start () {
+
+		//Test to see if we can access the values of the Level Manager
+		Debug.Log(LevelManager.lm.test);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("e")) {
+			presentTime = !presentTime;
+			//Debug.Log("Time shifting! Present time = " + presentTime);
+			changeTime (presentTime);
+		}
+	}
+
+	public void changeTime (bool time) {
+		LevelManager.lm.changeTime (time);
+	}
+
+
+}
