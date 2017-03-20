5630793526f6b3f432f9c87a7ed12a9f67889733
nstovring <nstovring@live.dk>
2017-03-20 17:32:03 +0100

    Some climbing added

---
+++ Assets/CharacterAnimator.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimator : MonoBehaviour
+{
+    public Animator myAnimator;
+    public CharacterMotor myMotor;
+	// Use this for initialization
+	void Start ()
+	{
+	    //myAnimator.applyRootMotion = false;
+	}
+
+    private float HSpeed;
+    public void SetVelocity(Vector3 CharVelocity)
+    {
+        HSpeed = Mathf.Lerp(HSpeed,CharVelocity.normalized.x * Mathf.Sign(CharVelocity.x),0.01f);
+        myAnimator.SetFloat("HSpeed", CharVelocity.normalized.sqrMagnitude);
+    }
+
+    private float VSpeed;
+    public void SetVSpeed(float speed)
+    {
+        //VSpeed = Mathf.Lerp(VSpeed, speed, 0.5f);
+
+        myAnimator.SetFloat("VSpeed", speed);
+    }
+
+    public void SetDistFromGround(float dist)
+    {
+        myAnimator.SetFloat("DistFromGround", dist);
+    }
+    public void Jump(bool jumping)
+    {
+        myAnimator.SetBool("Jumping", jumping);
+    }
+
+    public void HangOnLedge(bool hanging)
+    {
+        myAnimator.SetBool("IsHanging", hanging);
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
--- Assets/CharacterMotor.cs
+++ Assets/CharacterMotor.cs
@@ -2,25 +2,126 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterMotor : MonoBehaviour {
-
+public class CharacterMotor : MonoBehaviour
+{
+    public CharacterAnimator myCharAnimator;
     public float speed = 6.0F;
     public float jumpSpeed = 8.0F;
     public float gravity = 20.0F;
     private Vector3 moveDirection = Vector3.zero;
+    private CharacterController controller;
+
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        //StartCoroutine(AnimationQueues());
+    }
+
+
+    private bool IsFalling()
+    {
+        if ((int) Mathf.Sign(controller.velocity.y*2) == -1)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public bool wasInAir = false;
     void Update()
     {
-        CharacterController controller = GetComponent<CharacterController>();
-        if (controller.isGrounded)
+        Movement();
+    }
+    float DistanceFromGround()
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, -transform.up);
+        if (Physics.Raycast(ray, out hit))
         {
+            return Vector3.Distance(transform.position, hit.point);
+        }
+        return 0;
+    }
+    bool isHanging = false;
+
+    public void Movement()
+    {
+        //if (DistanceFromGround() < 0.4 && IsFalling())
+        //{
+        //    myCharAnimator.SetVSpeed(1);
+        //    Debug.Log("Landing");
+        //}
+        myCharAnimator.SetDistFromGround(DistanceFromGround());
+        if (isHanging)
+        {
+            if (Input.GetButton("Jump"))
+            {
+                isHanging = false;
+                myCharAnimator.HangOnLedge(false);
+            }
+        }
+        if (DistanceFromGround()< 0.1f && !isHanging)
+        {
+            if (wasInAir)
+            {
+                myCharAnimator.SetVSpeed(1);
+                Debug.Log("Landing");
+                
+                wasInAir = false;
+                //StartCoroutine(StopJumpingDelayed(0.01f));
+            }
+            myCharAnimator.Jump(false);
             moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            moveDirection = transform.TransformDirection(moveDirection);
+            if (moveDirection.magnitude >= 0.1)
+                transform.rotation = Quaternion.LookRotation(moveDirection);
+            // moveDirection = transform.TransformDirection(moveDirection);
             moveDirection *= speed;
             if (Input.GetButton("Jump"))
+            {
+                myCharAnimator.Jump(true);
                 moveDirection.y = jumpSpeed;
-
+                myCharAnimator.SetVSpeed(0);
+                Debug.Log("Jumping");
+                wasInAir = true;
+            }
         }
-        moveDirection.y -= gravity * Time.deltaTime;
-        controller.Move(moveDirection * Time.deltaTime);
+        else
+        {
+            //myCharAnimator.Jump(true);
+            if (IsFalling() && !isHanging)
+            {
+                myCharAnimator.SetVSpeed(0.5f);
+                Debug.Log("Idle Air");
+            }
+            //myCharAnimator.SetVSpeed();
+        }
+
+        //myCharAnimator.SetJumpSpeed(controller.velocity.normalized.y);
+        if (!isHanging)
+        {
+            moveDirection.y -= gravity*Time.deltaTime;
+            controller.Move(moveDirection*Time.deltaTime);
+            myCharAnimator.SetVelocity(controller.velocity);
+        }
     }
+
+    public IEnumerator StopJumpingDelayed(float delay)
+    {
+        yield return new WaitForSecondsRealtime(delay);
+        myCharAnimator.Jump(false);
+        wasInAir = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.transform.name);
+        if (other.transform.tag == "Ledge")
+        {
+            myCharAnimator.HangOnLedge(true);
+            isHanging = true;
+        }
+        Destroy(other.gameObject);
+    }
+
 }
