e0e0c6fa6b33562b48534f8052f287f0920272e8
Nikolaj Støvring <nstovring@live.dk>
2017-03-21 13:44:00 +0100

    Levitation Transparency & Guard

---
+++ Assets/GuardMotor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardMotor : MonoBehaviour {
+    public List<Transform> PatrolPoints;
+
+
+    public Animator myAnimator;
+	// Use this for initialization
+	void Start () {
+        myAnimator.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
