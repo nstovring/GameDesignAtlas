e76cdc45b8ac9d5e4d2a0a0fcf0d4733ca9558e7
JeppeKorsholm <jeppe.korsholm@hotmail.com>
2017-03-22 15:27:57 +0100

    added interactable classes

---
+++ Assets/ObjectiveItem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectiveItem : InteractableObject {
+    public override void Interact()
+    {
+        base.Interact();
+        
+
+
+        LevelManager.lm.keyscollected += 1; //rename to objectives Collected
+        Debug.Log("key collected! nmb: " + LevelManager.lm.keyscollected);
+        Destroy(this.gameObject, 0.1f);
+        
+
+    }
+}
---
+++ Assets/Scripts/SphereItem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereItem : InteractableObject {
+
+
+    public override void Interact()
+    {
+        base.Interact();
+
+
+
+        LevelManager.lm.spherescollected += 1; //rename to objectives Collected
+        Debug.Log("sphere collected! nmb: " + LevelManager.lm.spherescollected);
+        Destroy(this.gameObject, 0.1f);
+
+
+    }
+}
