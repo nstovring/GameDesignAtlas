2b8387addea32b1228cbc9f12dab3a96448b60f0
JeppeKorsholm <jeppe.korsholm@hotmail.com>
2017-03-22 16:31:49 +0100

    updated guard prefab

---
+++ Assets/ThreatObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThreatObject : InteractableObject {
+    public override void Interact()
+    {
+        base.Interact();
+
+
+        LevelManager.lm.restartLevel();
+
+
+    }
+}
