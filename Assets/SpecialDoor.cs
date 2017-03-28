d4c21d78ea4eb51e83230653f837c307e844cc5e
Nikolaj Støvring <nstovring@live.dk>
2017-03-28 14:55:14 +0200

    DoorRoom animations

---
+++ Assets/SpecialDoor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpecialDoor : MonoBehaviour, IInteractable {
+
+    public int currentUnlockedLocks = 0;
+
+    public void Interact()
+    {
+        currentUnlockedLocks = LevelManager.lm.keyscollected;
+        OpenLock();
+    }
+
+    public Animator animator;
+
+    void OpenLock()
+    {
+        animator.SetInteger("OpenedLocks", currentUnlockedLocks);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
