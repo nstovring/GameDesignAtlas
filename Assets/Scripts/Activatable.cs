70427350acaa170832a6ac4b8f405bdb157a65f5
Nikolaj Støvring <nstovring@live.dk>
2017-03-31 13:52:58 +0200

    Intro Scene Updates

--- Assets/Scripts/Activatable.cs
+++ Assets/Scripts/Activatable.cs
@@ -9,10 +9,22 @@ public class Activatable : MonoBehaviour, ILinkable
     public void Activate()
     {
 
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.AddComponent<Rigidbody>();
+        }
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).parent = null;
+        }
+
         gameObject.SetActive(false);
+
     }
 
-   void Update()
+    void Update()
     {
 
         if (transform.childCount == 0)
