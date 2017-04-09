9ce23c64816429858be44043939f2de004db1f30
JeppeKorsholm <jeppe.korsholm@hotmail.com>
2017-04-09 16:33:19 +0200

    SplinesShouldWork

---
+++ Assets/Scripts/SplineWalker.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplineWalker : MonoBehaviour {
+
+    public SplineWalkerMode mode;
+    public BezierSpline spline;
+
+    private bool goingForward = true;
+    public float progress;
+    public float HSpeed;
+    public bool lookForward;
+
+    private void Update()
+    {
+        HSpeed = Input.GetAxis("Horizontal");
+        if (goingForward)
+        {
+            progress += Time.deltaTime * HSpeed;
+            if (progress > 1f)
+            {
+                if (mode == SplineWalkerMode.Once)
+                {
+                    progress = 1f;
+                }
+                else if (mode == SplineWalkerMode.Loop)
+                {
+                    progress -= 1f;
+                }
+                else
+                {
+                    progress = 2f - progress;
+                    goingForward = false;
+                }
+            }
+        }
+        else
+        {
+            progress -= Time.deltaTime / HSpeed;
+            if (progress < 0f)
+            {
+                progress = -progress;
+                goingForward = true;
+            }
+        }
+
+        Vector3 position = spline.GetPoint(progress);
+        transform.localPosition = position;
+        if (lookForward)
+        {
+            transform.LookAt(position + spline.GetDirection(progress));
+        }
+    }
+}
