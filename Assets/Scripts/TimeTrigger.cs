f13f763eaaed2a4608251d35e326b029adcd3877
Nikolaj Støvring <nstovring@live.dk>
2017-04-11 16:50:51 +0200

    Added Vegetation + Sounds

---
+++ Assets/Scripts/TimeTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeTrigger : MonoBehaviour {
+
+    public Transform pastObjects;
+    public Transform presentObjects;
+
+    public void OnTriggerEnter(Collider other)
+    {
+
+        //cameraMover.newSpeed = 0.2f;
+        //cameraMover.offset = cameraOffset;
+        //light.transform.rotation = Quaternion.Euler(lightRotation);
+
+        pastObjects.gameObject.SetActive(false);
+        presentObjects.gameObject.SetActive(true);
+    }
+}
